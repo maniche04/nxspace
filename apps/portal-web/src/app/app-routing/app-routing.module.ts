839136dfc891e995954b02de72191f8630d3ade1
Manish Bhattarai <manish.ilam@gmail.com>
2018-03-22 00:21:59 +0400

    adjust main toolbar

--- apps/portal-web/src/app/app-routing/app-routing.module.ts
+++ apps/portal-web/src/app/app-routing/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: '',
