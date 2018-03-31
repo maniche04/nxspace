379fbb12f82e3b5c01c9f559da2c3463d0d19a2a
Manish Bhattarai <manish@jizanperfumes.com>
2018-03-31 19:19:10 +0400

    home page and products

--- apps/portal-web/src/app/home/home.component.ts
+++ apps/portal-web/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ProductComponent } from './../product/product.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products = [
+    {
+      name:'ABERCROMBIE & FITCH FIRST INSTINCT (L) EDP 50 ml',
+      price:'55',
+      image:'https://res.cloudinary.com/hamropic/image/upload/c_thumb,w_200/v1494149474/jvs2bzb6hmc6kuhfbprh.jpg'
+    },
+    {
+      name: 'ACQUA DI PARMA BLU M/G DI SARDEGNA (U) B/L 200 ml',
+      price:'100',
+      image:'https://res.cloudinary.com/hamropic/image/upload/c_thumb,w_200/v1504683159/isab139rtwndsqfehkym.jpg'
+    },
+    {
+      name: 'ALEXANDRE J THE COLL GOLDEN OUD (U) EDP TEST 100',
+      price:'250',
+      image:'https://res.cloudinary.com/hamropic/image/upload/c_thumb,w_200/v1519039491/djeoonst98liemwx3iib.jpg'
+    },
+    {
+      name:'ABERCROMBIE & FITCH FIRST INSTINCT (L) EDP 50 ml',
+      price:'55',
+      image:'https://res.cloudinary.com/hamropic/image/upload/c_thumb,w_200/v1494149474/jvs2bzb6hmc6kuhfbprh.jpg'
+    },
+    {
+      name: 'ACQUA DI PARMA BLU M/G DI SARDEGNA (U) B/L 200 ml',
+      price:'100',
+      image:'https://res.cloudinary.com/hamropic/image/upload/c_thumb,w_200/v1504683159/isab139rtwndsqfehkym.jpg'
+    },
+    {
+      name: 'ALEXANDRE J THE COLL GOLDEN OUD (U) EDP TEST 100',
+      price:'250',
+      image:'https://res.cloudinary.com/hamropic/image/upload/c_thumb,w_200/v1519039491/djeoonst98liemwx3iib.jpg'
+    }
+  ]
+
   constructor() {}
 
   ngOnInit() {}
